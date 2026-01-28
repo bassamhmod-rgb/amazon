d1ddb0196299193a4677de77d564f325d9b82652
bassam hmod <bassamhmoud8@gmail.com>
2026-01-28 15:22:58 +0300

    Fix order confirm bug

--- multistore/settings.py
+++ multistore/settings.py
@@ -4,7 +4,27 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "change-me"
 
-DEBUG = True
+#DEBUG = True
+ADMINS = [("Admin", "admin@example.com")]
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "ERROR",
+            "class": "logging.FileHandler",
+            "filename": BASE_DIR / "django_errors.log",
+
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+    },
+}
 
 ALLOWED_HOSTS = [
     "amazonsyria.pythonanywhere.com",
