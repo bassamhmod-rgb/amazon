91e4d8125627aad0a71157d26bb257f08d16dd76
bassam hmod <bassamhmoud8@gmail.com>
2026-01-29 13:34:30 +0300

    Fix order confirm bug

--- stores/admin.py
+++ stores/admin.py
@@ -3,7 +3,7 @@ from .models import Store
 
 @admin.register(Store)
 class StoreAdmin(admin.ModelAdmin):
-    list_display = ("name", "owner", "mobile", "theme", "is_active")
+    list_display = ("name", "owner", "mobile", "rkmdb", "rkmtb", "theme", "is_active")
     search_fields = ("name", "owner__username")
     list_filter = ("theme", "is_active")
 
